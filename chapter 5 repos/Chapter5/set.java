5dfbf19ac94d05bab06666c572efd6e509862d6f
kevinthayes <khayes@naperville203.org>
2019-11-22 09:09:35 -0600

    added set interface

---
+++ chapter 5 repos/Chapter5/set.java
@@ -0,0 +1,9 @@
+
+
+public interface set
+{
+public void setState (String password, String st)
+public void setAddress( Address obj)
+public void setAddress(String street, String city, int zip)
+
+}
